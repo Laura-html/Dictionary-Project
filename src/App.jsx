a84e9425a6de60fa5acf4f864ea5bbcde7c2c8f9
Laura-html <lrichards023@gmail.com>
2026-05-30 05:55:50 +0100

    Added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Hello World</h1>;
-  <a href="#" className="btn btn-primary">
-    Hello
-  </a>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header>Logo</header>
+        <h1>Hello World</h1>;
+        <a href="#" className="btn btn-primary">
+          Hello
+        </a>
+        <footer>
+          Coded by Laura, open-sourced on Github and hosted by Netlify.
+        </footer>
+      </div>
+    </div>
+  );
 }
